f7df42060d37fe5e6c80e77c0b82d857110075e9
Santiago Acosta <122395802+saacostam-zaga@users.noreply.github.com>
2023-12-28 00:58:37 -0500

    [FEAT] Enhance Factories (#4)
    * enhance factories
    * build gh-pages

--- src/object/enviroment/tile/track.factory.ts
+++ src/object/enviroment/tile/track.factory.ts
@@ -11,7 +11,7 @@ const trackTileObjectsConstructors = [
     Car,
 ]
 
-export const TrackTileFactory: TileFactory = (scene: BaseScene, _: number, tile: Tile): Entity[] => {
+export const TrackTileFactory: TileFactory = (scene: BaseScene, amount: number, tile: Tile): Entity[] => {
     const factoryEntityProducts: Entity[] = [];
 
     const direction: Direction = Math.random() < 0.5 ? 'right' : 'left';
@@ -25,18 +25,30 @@ export const TrackTileFactory: TileFactory = (scene: BaseScene, _: number, tile:
     const start = new Vector3( tile._mesh.position.x, HEIGHT/2, (direction === 'right') ? MIN : MAX);
     const end = new Vector3( tile._mesh.position.x, HEIGHT/2, (direction === 'right') ? MAX : MIN);
 
+    const velocity = 0.00012 + (Math.random() * 0.00006)
+
+    start.y += HEIGHT/8;
+    end.y += HEIGHT/8;
+
+    let currPathProgress = Math.random();
     const TrackObjectConstructor = trackTileObjectsConstructors[Math.floor(trackTileObjectsConstructors.length * Math.random())];
+    
+    for (let iter = 0; iter <= amount; iter++){
+        const entity = new TrackObjectConstructor(scene, {
+            direction: direction,
+            start: start,
+            end: end,
+            width: SIZE,
+            depth: SIZE*3,
+            height: HEIGHT,
+            pathProgress: currPathProgress,
+            velocity: velocity,
+        });
+    
+        factoryEntityProducts.push(entity);
 
-    const entity = new TrackObjectConstructor(scene, {
-        direction: direction,
-        start: start,
-        end: end,
-        width: SIZE,
-        depth: SIZE*3,
-        height: HEIGHT,
-    });
-
-    factoryEntityProducts.push(entity);
+        currPathProgress = (currPathProgress + 1/amount) % 1;
+    }
 
     return factoryEntityProducts;
 }
